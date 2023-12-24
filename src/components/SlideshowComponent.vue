e705096e82a80beaffbf82c9f43eb21b4987b676
AmedeoPioSpina <spina.amedeo.pio@gmail.com>
2023-12-24 12:09:26 +0100

    sistemare chiamata axios

--- src/components/SlideshowComponent.vue
+++ src/components/SlideshowComponent.vue
@@ -1,13 +1,27 @@
 <template lang="">
     <section class="slideshow-container">
-        
+        <button @click="axiosMoviesFunc()"></button>
     </section>
 </template>
 <script>
+import axios from 'axios';
 export default {
     name: "SlideshowComponent",
     components: {
 
+    },
+    data() {
+        return {
+            moviesList: [],
+        }
+    },
+    methods: {
+        axiosMoviesFunc(){
+            console.log("hello")
+            axios.get("https://api.themoviedb.org/3/movie/299534?api_key=a2cb9b8358fbf60f421cced84a3dd562&language=it-IT&query=Avengers:%20Endgame")
+            .then(resp => this.moviesList= [resp.data])
+            .catch(err => console.warn(err));
+        },
     }
 }
 </script>
